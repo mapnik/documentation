b567965fa0e0523e321a07473d8e4c59f683f653
mapsam <matthews.sam@gmail.com>
2016-05-31 16:58:30 -0700

    fresh clone, ssh style

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,54 @@
 # set -o errexit -o nounset
 
 echo $TRAVIS_BRANCH
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
 
-if [ "$TRAVIS_BRANCH" != "gh-pages" ]
+if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 then
   echo "Not a commit that we want to build. Only commits to the gh-pages branch will build."
   exit 0
 fi
 
-rev=$(git rev-parse --short HEAD)
-echo $rev
+echo " - - - BRANCH VARS - - - "
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+echo $REPO
+echo $SSH_REPO
+echo $SHA
 
-ls -lah
+# since we are doing this from the master branch, let's 
+#  get a fresh clone ready, and set it to gh-pages
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH
+cd ..
 
-git init
+# copy over the newly rolled docs, from generate.sh
+cp -r node-mapnik/ out/
+
+# now go back into our fresh clone, with updated files
+# and start getting things ready for the push
+cd out
 git config user.name "$GH_USERNAME"
 git config user.email "$GH_EMAIL"
-git remote add upstream "https://$GH_TOKEN@github.com/mapnik/documentation.git"
-git fetch upstream
-git reset upstream/gh-pages
 
-git add .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+echo " - - - GIT STATUS - - - "
+git status
+
+git add --all
+git commit -m "Update docs at ${SHA}"
+
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push $SSH_REPO $TARGET_BRANCH
 
 exit 0
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,13 +7,14 @@ echo "VERSION: ${VERSION}"
 touch testfile.txt
 
 ls -lah
-# cd node_modules/documentation
-# npm link
-# cd ../..
 
-# CONFIG=config/node.json
-# THEME=node_modules/node-mapnik-theme/
-# SOURCE=node_modules/mapnik/src/*.cpp
-# SINK=./node-mapnik/${VERSION}
+cd node_modules/documentation
+npm link
+cd ../..
 
-# documentation build ${SOURCE} --polyglot -f html -o ${SINK} --github --name "Node Mapnik" --theme ${THEME} --config ${CONFIG}
+CONFIG=config/node.json
+THEME=node_modules/node-mapnik-theme/
+SOURCE=node_modules/mapnik/src/*.cpp
+SINK=./node-mapnik/${VERSION}
+
+documentation build ${SOURCE} --polyglot -f html -o ${SINK} --github --name "Node Mapnik" --theme ${THEME} --config ${CONFIG}
