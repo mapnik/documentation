042b1a8293d9e6b242c11c7b05c34c42379d6ced
mapsam <matthews.sam@gmail.com>
2016-05-31 15:21:13 -0700

    echo chambers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 set -o errexit -o nounset
 
+echo $TRAVIS_BRANCH
+
 if [ "$TRAVIS_BRANCH" != "gh-pages" ]
 then
   echo "Not a commit that we want to build. Only commits to the gh-pages branch will build."
@@ -9,6 +11,7 @@ then
 fi
 
 rev=$(git rev-parse --short HEAD)
+echo $rev
 
 git init
 git config user.name "$GH_USERNAME"
@@ -20,3 +23,5 @@ git reset upstream/gh-pages
 git add .
 git commit -m "rebuild pages at ${rev}"
 git push -q upstream HEAD:gh-pages
+
+exit 0
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,13 +1,15 @@
 # generates Node Mapnik documentation using documentationjs
+
+
 VERSION=$1
+echo "VERSION: ${VERSION}"
+# cd node_modules/documentation
+# npm link
+# cd ../..
 
-cd node_modules/documentation
-npm link
-cd ../..
+# CONFIG=config/node.json
+# THEME=node_modules/node-mapnik-theme/
+# SOURCE=node_modules/mapnik/src/*.cpp
+# SINK=./node-mapnik/${VERSION}
 
-CONFIG=config/node.json
-THEME=node_modules/node-mapnik-theme/
-SOURCE=node_modules/mapnik/src/*.cpp
-SINK=./node-mapnik/${VERSION}
-
-documentation build ${SOURCE} --polyglot -f html -o ${SINK} --github --name "Node Mapnik" --theme ${THEME} --config ${CONFIG}
+# documentation build ${SOURCE} --polyglot -f html -o ${SINK} --github --name "Node Mapnik" --theme ${THEME} --config ${CONFIG}
