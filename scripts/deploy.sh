eb61dd504c73d8ba5d51154d0cdda63cbfa4be21
mapsam <matthews.sam@gmail.com>
2016-05-31 17:32:15 -0700

    try https again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,10 +14,8 @@ fi
 
 echo " - - - BRANCH VARS - - - "
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 echo $REPO
-echo $SSH_REPO
 echo $SHA
 
 # since we are doing this from the master branch, let's 
@@ -43,15 +41,15 @@ git status
 git add --all # dangerous?
 git commit -m "Update docs at ${SHA}"
 
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+# chmod 600 deploy_key
+# eval `ssh-agent -s`
+# ssh-add deploy_key
 
-git push $SSH_REPO $TARGET_BRANCH
+git push "https://$GITHUB_AUTH@github.com/rust-lang/rust-by-example.git" $TARGET_BRANCH
 
 exit 0
