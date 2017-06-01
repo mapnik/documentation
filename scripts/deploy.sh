fe36b78ddd2c48312f8fed34bafc1faa678ed412
mapsam <matthews.sam@gmail.com>
2017-06-01 11:46:17 -0700

    proper pwd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ TARGET_BRANCH="gh-pages"
 
 if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 then
-  echo "Not a commit that we want to build. Only commits to the gh-pages branch will build."
+  echo "Not a commit that we want to build. Only commits to the $SOURCE_BRANCH branch will build."
   exit 0
 fi
 
@@ -16,13 +16,11 @@ SHA=`git rev-parse --verify HEAD`
 
 # since we are doing this from the master branch, let's
 #  get a fresh clone ready, and set it to gh-pages
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH
 cd ..
+git clone $REPO out && cd out && git checkout $TARGET_BRANCH && cd ..
 
-# copy over the newly rolled docs, from scripts/generate.sh
-cp -r ./ out/
+# copy over the newly rolled docs from scripts/generate.sh to the /out directory
+cp -r documentation out
 
 # now go back into our fresh clone, with updated files
 # and start getting things ready for the push
