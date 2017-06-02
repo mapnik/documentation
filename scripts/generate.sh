6c294414406bdb05859eaf8b0aa9bedd4aa319d8
Sam Matthews <matthews.sam@gmail.com>
2017-06-02 08:59:50 -0700

    Fix commit links (#11)
    * Update docs at bd593b09f63630f36088e31acfea532f95d44924
    
    * Update docs at 88e3185396f7754eb937190dab737fe2b44d0e15
    
    * Update docs at e6bb97714cdaa472991f088a887ef8458daa5feb
    
    * remove cruft
    
    * Update docs at 77081af5431581be255efbb843f621a7be17d3b5
    
    * adding old 3.4 docs
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at 2007848f1b4b49be176f9d9542d822372ef471ec
    
    * Update docs at ddeaaa1dc5a1e7402ca32231d1b3fff95e227a74
    
    * Update docs at ddeaaa1dc5a1e7402ca32231d1b3fff95e227a74
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at ab56f71bfdf76228db5c693cd4f29f65a5052f96
    
    * Update docs at b5dc709522afe9226f4156c9a0270137f30a698e
    
    * Update docs at fe36b78ddd2c48312f8fed34bafc1faa678ed412
    
    * Update docs at 28f162def541a41c461b16567dc698d1f2c2bc54
    
    * remove accidental docs folder
    
    * include theme directly in docs, clone repo instead of using npm, grab gitsha
    
    * remove crufty theme code
    
    * update versions with new links
    
    * fix README conflict
    
    * remove mapnik from package.json
    
    * run this on node 6
    
    * but really

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,24 +1,43 @@
-# generates Node Mapnik documentation using documentationjs
+# Generates Node Mapnik documentation by cloning the node-mapnik
+# repository and running documentationjs commands
+set -eu
 
-# get the full version and parse out the minor version
-FULL_VERSION=$(node -e "console.log(require('./node_modules/mapnik/package.json').version)")
+REPO="https://github.com/mapnik/node-mapnik.git"
+BRANCH="master"
+
+# clone node-mapnik master to get current gisha and minor version
+# this is used to build proper source links in the documentation
+# and set the minor version (i.e. 3.5) for building a proper
+# directory for docs to live
+TMP_REPO=./tmp-node-mapnik-repo
+git clone --branch $BRANCH $REPO $TMP_REPO --depth 1 && cd $TMP_REPO
+NODE_MAPNIK_GITSHA=`git rev-parse --verify HEAD`
+FULL_VERSION=$(node -e "console.log(require('./package.json').version)")
 MINOR_VERSION=$(node -e "console.log('$FULL_VERSION'.slice(0, '$FULL_VERSION'.lastIndexOf('.')))")
 echo "Building $MINOR_VERSION from mapnik version $FULL_VERSION"
-
-# make sure the documentation CLI works
-cd node_modules/documentation
-npm link
-cd ../..
-
-# documentation.js arguments
-CONFIG=config/node.json
-THEME=node_modules/node-mapnik-theme/
-SOURCE=node_modules/mapnik/src/*.cpp
-SINK=./node-mapnik/${MINOR_VERSION}
+cd ../
 
 # build the documentation in the folder named after the minor version
-documentation build ${SOURCE} --polyglot -f html -o ${SINK} --github --name "Node Mapnik" --theme ${THEME} --config ${CONFIG}
+# documentation.js arguments
+CONFIG=config/node.json
+THEME=./theme
+SOURCE=$TMP_REPO/src/*.cpp
+SINK=./node-mapnik/${MINOR_VERSION}
+
+NODE_MAPNIK_GITSHA=$NODE_MAPNIK_GITSHA ./node_modules/documentation/bin/documentation.js build ${SOURCE} \
+  --polyglot \
+  -f html \
+  -o ${SINK} \
+  --github \
+  --name "Node Mapnik" \
+  --theme ${THEME} \
+  --config ${CONFIG}
 
 # regenerate the index.html file to forward to the most recent minor version
 HTML="<!DOCTYPE html><html><head><meta http-equiv='content-type' content='text/html; charset=utf-8' /><meta http-equiv='refresh' content='0;url=$MINOR_VERSION/' /></head></html>"
 echo $HTML > ./node-mapnik/index.html
+
+# remove tmp node-mapnik repository cloned above
+rm -rf ./tmp-node-mapnik-repo
+
+exit 0
---
+++ theme/index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var fs = require('fs'),
+  path = require('path'),
+  File = require('vinyl'),
+  vfs = require('vinyl-fs'),
+  concat = require('concat-stream'),
+  Handlebars = require('handlebars'),
+  autolink = require('./lib/autolink'),
+  mapnik = require('child_process'),
+  formatMarkdown = require('./lib/format_markdown'),
+  formatParameters = require('./lib/format_parameters');
+
+module.exports = function (comments, options, callback) {
+  var pageTemplate = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'index.hbs'), 'utf8'));
+
+  Handlebars.registerPartial('section',
+    Handlebars.compile(fs.readFileSync(path.join(__dirname, 'section.hbs'), 'utf8'), {
+      preventIndent: true
+    })
+  );
+
+  var paths = comments.map(function (comment) {
+    return comment.path.join('.');
+  }).filter(function (path) {
+    return path;
+  });
+
+  // builds a proper blob URI to point to the source code
+  // NODE_MAPNIK_GITSHA is provided from /scripts/generate.sh
+  Handlebars.registerHelper('mapnik_gitsha', function(url) {
+    var base = 'https://github.com/mapnik/node-mapnik/blob';
+    var gitsha = process.env.NODE_MAPNIK_GITSHA || 'master';
+    var source = url.slice(url.indexOf('src/'), -1);
+    return `${base}/${gitsha}/${source}`;
+  });
+
+  Handlebars.registerHelper('mapnik_srcpath', function(url) {
+    return url.slice(url.indexOf('/src/'));
+  });
+
+  Handlebars.registerHelper('permalink', function(url) {
+    return url;
+  });
+
+  Handlebars.registerHelper('permalink', function () {
+    return this.path.join('.');
+  });
+
+  Handlebars.registerHelper('autolink', function (text) {
+    return new Handlebars.SafeString(autolink(paths, text));
+  });
+
+  Handlebars.registerHelper('format_params', formatParameters);
+
+  Handlebars.registerHelper('md', function (string) {
+    return new Handlebars.SafeString(formatMarkdown(string, paths));
+  });
+
+  Handlebars.registerHelper('format_type', function (type) {
+    return new Handlebars.SafeString(formatMarkdown.type(type, paths));
+  });
+
+  Handlebars.registerHelper('trunk', function (string) {
+    return string.substr(0, 100) + ' ...';
+  });
+
+  var highlight = require('./lib/highlight')(options.hljs || {});
+  Handlebars.registerHelper('highlight', function (string) {
+    return new Handlebars.SafeString(highlight(string));
+  });
+
+  // push assets into the pipeline as well.
+  vfs.src([__dirname + '/assets/**'], { base: __dirname })
+    .pipe(concat(function (files) {
+      callback(null, files.concat(new File({
+        path: 'index.html',
+        contents: new Buffer(pageTemplate({
+          docs: comments,
+          options: options
+        }), 'utf8')
+      })));
+    }));
+};
